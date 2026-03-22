ae861f2077d6b3a827a6a9a7cee405f3a1e8ab85
elzsh <ilyass.elzsh@gmail.com>
2026-03-22 17:22:06 -0400

    refactor(cpu): cleanup

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,5 +1,7 @@
 #include "cpu.h"
 
+#include "types.h"
+
 #include <cstdio>
 #include <cstdlib>
 
@@ -8,16 +10,24 @@ void CPU::reset() {
     next_pc = pc + 4;
 }
 
-void CPU::decode_and_execute(u32 instruction) {
-    std::fprintf(stderr, "Unhandled instruction %08x\n", instruction);
-    std::abort();
+void CPU::decode_and_execute(Instruction inst) {
+    switch (inst.op()) {
+        case 0x0F:    // lui
+            std::fprintf(stderr, "What now?\n");
+            std::abort();
+            break;
+        default:
+            std::fprintf(stderr, "Unhandled instruction %08x\n", inst.bits);
+            std::abort();
+            break;
+    }
 }
 
 void CPU::tick() {
-    u32 instruction = bus->load32(pc);
+    Instruction inst(bus->load32(pc));
 
     pc = next_pc;
     next_pc += 4;
 
-    decode_and_execute(instruction);
+    decode_and_execute(inst);
 }
