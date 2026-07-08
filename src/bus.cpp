79bbd8c567ea7255ccd95cbeda33b264d37bf328
elzsh <ilyass.elzsh@gmail.com>
2026-07-07 22:09:33 -0400

    save last changes

--- src/bus.cpp
+++ src/bus.cpp
@@ -7,27 +7,37 @@
 
 Bus::Bus() {
     read_handlers.fill(&unmapped_read);
+    write_handlers.fill(&unmapped_write);
 }
 
-void Bus::set_handler(u32 base, u32 size, ReadHandler read_handler) {
+void Bus::set_handler(u32 base, u32 size, ReadHandler rd, WriteHandler wr) {
     u32 start_page = base >> PAGE_SHIFT;
     u32 num_pages = (size + (1 << PAGE_SHIFT) - 1) >> PAGE_SHIFT;
 
     for (u32 i = 0; i < num_pages; i++) {
-        read_handlers[start_page + i] = read_handler;
+        read_handlers[start_page + i] = rd;
+        write_handlers[start_page + i] = wr;
     }
 }
 
 void Bus::map_bios(std::span<u8> bios_data) {
     bios = bios_data;
-    set_handler(BIOS::BASE, BIOS::SIZE, &bios_read);
+    set_handler(BIOS::BASE, BIOS::SIZE, &bios_read, &ignore_write);
 }
 
+/* Unmapped & Sink */
 u32 Bus::unmapped_read(Bus* /*bus*/, u32 address) {
-    std::fprintf(stderr, "Unhandled load32 at address %08x\n", address);
+    std::fprintf(stderr, "Unhandled load32 at address 0x%08x\n", address);
     return 0xFFFFFFFF;
 }
 
+void Bus::unmapped_write(Bus* /*bus*/, u32 address, u32 value) {
+    std::fprintf(stderr, "Unhandled store32: val 0x%08x to addr 0x%08x\n", value, address);
+}
+
+void Bus::ignore_write(Bus* /*bus*/, u32 /*address*/, u32 /*value*/) { /* KEEP EMPTY */ }
+
+/* BIOS */
 u32 Bus::bios_read(Bus* bus, u32 address) {
     u32 offset = address & BIOS::MASK;
     u32 value;
@@ -39,4 +49,9 @@ u32 Bus::bios_read(Bus* bus, u32 address) {
     }
 
     return value;
+}
+
+/* MEMCTRL */
+void Bus::memctrl_write(Bus* /*bus*/, u32 address, u32 value) {
+    std::fprintf(stderr, "MEMCTRL write: val 0x%08x to addr 0x%08x\n", value, address);
 }
--- src/bus.h
+++ src/bus.h
@@ -12,23 +12,24 @@ struct Bus {
     constexpr static u32 PAGE_COUNT = 0x20000;    // Top 17 bits of the 29 bit address
 
     using ReadHandler = u32 (*)(Bus* bus, u32 address);
+    using WriteHandler = void (*)(Bus* bus, u32 address, u32 value);
 
     Bus();
 
     u32 load32(u32 address) {
         // if the two LSB are not 00
-        if ((address & 0x3) != 0) {
+        if ((address & 0x3) != 0) [[unlikely]] {
             std::fprintf(stderr, "Unaligned load32 address: 0x%08x\n", address);
             std::abort();
         }
 
-        u32 physical = address & 0x1FFFFFFF;    // Drop top 3 bits
+        u32 physical = address & 0x1FFFFFFF;    // Drop KSEG/KUSEG bits
         return read_handlers[physical >> PAGE_SHIFT](this, address);
     }
 
     void store32(u32 address, u32 val) {
         // if the two LSB are not 00
-        if ((address & 0x3) != 0) {
+        if ((address & 0x3) != 0) [[unlikely]] {
             std::fprintf(stderr, "Unaligned store32 address: 0x%08x\n", address);
             std::abort();
         }
@@ -48,9 +49,18 @@ struct Bus {
 private:
     std::span<u8> bios;
     std::array<ReadHandler, PAGE_COUNT> read_handlers;
+    std::array<WriteHandler, PAGE_COUNT> write_handlers;
 
-    void set_handler(u32 base, u32 size, ReadHandler read_handler);
+    void set_handler(u32 base, u32 size, ReadHandler rd, WriteHandler wr);
 
+    /* Unmapped & Sink */
     static u32 unmapped_read(Bus* bus, u32 address);
+    static void unmapped_write(Bus* bus, u32 address, u32 value);
+    static void ignore_write(Bus* bus, u32 address, u32 value);
+
+    /* BIOS */
     static u32 bios_read(Bus* bus, u32 address);
+
+    /* memctrl */
+    static void memctrl_write(Bus* bus, u32 address, u32 value);
 };
